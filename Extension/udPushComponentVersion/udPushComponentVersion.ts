86c6db953192b5d74b4c93df55aac47a03cc0a01
Lucas Killgore <lukillgo@microsoft.com>
2017-07-20 11:27:53 -0400

    #1035203: cleanup

--- Extension/udPushComponentVersion/udPushComponentVersion.ts
+++ Extension/udPushComponentVersion/udPushComponentVersion.ts
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-//import * as tl from 'vsts-task-lib/task';
 import tl = require('vsts-task-lib/task');
 import path = require('path');
 import process = require('process');
